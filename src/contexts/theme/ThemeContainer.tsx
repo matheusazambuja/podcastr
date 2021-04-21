a8ee744d66ec2e3266734053bf80d3a6bbb75f2e
matheusazambuja <matheusazambuja35@gmail.com>
2021-04-21 11:02:09 -0300

    initial theme ✔

---
+++ src/contexts/theme/ThemeContainer.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import theme from '../../styles/theme'
+
+interface ThemeContainerProps {
+  children: ReactNode
+}
+
+export default function ThemeContainer({ children }: ThemeContainerProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      {children}
+    </ChakraProvider>
+  )
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,82 @@
+import { theme, extendTheme } from '@chakra-ui/react'
+
+export const customTheme = extendTheme({
+  styles: {
+    global: {
+      '*': {
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box'
+      },
+
+      body: {
+        background: 'gray.50'
+      },
+
+      'body, input, textarea, button': {
+        font: '500 1rem Inter, sans-serif',
+        color: 'gray.500'
+      },
+
+      button: {
+        cursor: 'pointer'
+      },
+
+      'h1, h2, h3, h4, h5, h6': {
+        fontWeight: 600,
+        fontFamily: 'Lexend, sans-serif',
+        color: 'gray.800'
+      },
+
+      h1: {
+        fontSize: '2rem'
+      },
+
+      h2: {
+        fontSize: '1.5rem'
+      }
+    }
+  },
+  breakpoints: {
+    ...theme.breakpoints,
+  },
+
+  fontWeights: {
+    ...theme.fontWeights,
+  },
+
+  radii: {
+    ...theme.radii
+  },
+
+  fontSizes: {
+    ...theme.fontSizes
+  },
+
+  colors: {
+    ...theme.colors,
+    gray: {
+      ...theme.colors.gray,
+      50: '#F7F8FA',
+      100: '#E6E8EB',
+      200: '#AFB2B1',
+      500: '#808080',
+      800: '#494D4B',
+    },
+  
+    green: {
+      ...theme.colors.green,
+      500: '#04D361'
+    },
+  
+    purple: {
+      ...theme.colors.purple,
+      300: '#9F75FF',
+      400: '#9164FA', 
+      500: '#8257E5',
+      800: '#6F48C9',
+    }
+  }
+})
+
+export default customTheme
